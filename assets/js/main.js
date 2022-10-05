2dc6aa570cb3689a34e2a665c86d7d73d00d9998
Josue Garces Jouvin <josuejouvin27@gmail.com>
2022-10-05 18:55:49 -0500

    Portfolio falta editar

---
+++ assets/js/main.js
@@ -0,0 +1,94 @@
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show-menu')
+})
+
+navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+})
+
+//Remove menu mobile
+
+let navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{ 
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(link => link.addEventListener('click', linkAction))
+
+
+//QUALIFICATION TABS
+const tabs =  document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+//Filter portfolio
+let mixerPortfolio = mixitup('.work__container',{
+    selectors:{
+        target: '.work__card'
+    },
+    animation:{
+        duration: 300
+    }
+})
+
+//ACTIVE LINK
+const linkWork = document.querySelectorAll('.work__item');
+
+function activateWork() {
+    linkWork.forEach(link => link.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkWork.forEach(link => link.addEventListener('click', activateWork))
+
+
+
+//DARK MODE
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
